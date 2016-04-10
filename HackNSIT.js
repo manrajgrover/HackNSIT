ebd73e4d27817f444575b56638bfe972b287149b
Ayush Garg <ayush.nxs@gmail.com>
2016-04-10 09:20:17 +0530

    twitter chal raha

--- HackNSIT.js
+++ HackNSIT.js
@@ -15,9 +15,22 @@
 //     }
 //   });
 // }
+if (Meteor.isServer) {
+	Meteor.startup(function () {
+		console.log("HELLOO");
+		var Twitter = Meteor.npmRequire('twit');
 
-// if (Meteor.isServer) {
-//   Meteor.startup(function () {
-//     // code to run on server at startup
-//   });
-// }
+		var client1 = new TwitMaker({
+			consumer_key: '0AyxYs06mikk2Z0ZAvx0DlxZw',
+  			consumer_secret: 'qEIqwHat60rzeU33Gs7O6NIGaXmY8vNFOwX6W5NvZK6lyrhdlR',
+  			access_token: '2313261072-MTfmroVnvtSEM5Yvu6NcRvxmJ64CnTTYDlqF0F6',
+  			access_token_secret: 'HzwxdUfHsoui9hU8fYjTOSl9bXjpMSHYCHmA4qMtkS26v'
+  		});
+		// var stream = client1.stream('statuses/filter', {'locations':'-73,40,-74,41' });
+		var stream = client1.stream('statuses/filter', {track: 'googler'});
+		stream.on('tweet',function (tweet) {
+    		console.log(tweet);
+    	});
+  	});
+}
+
